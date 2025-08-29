b1d6f7be1467b339f732f650c8a6d5aa95abbcdd
Sopheackanha <hort.sopheackanha24@kit.edu.kh>
2025-08-29 14:35:02 +0700

    (っ °Д °;)っ

--- src/app/(quiz)/add-slide/page.tsx
+++ src/app/(quiz)/add-slide/page.tsx
@@ -18,21 +18,24 @@ export default function AddSlide() {
   };
 
   return (
-    <div className="min-h-screen">
+    <div>
       <div className="flex flex-col items-center mt-16">
         <h1 className="text-3xl font-bold text-[#1f2185] mb-10">Add Slide</h1>
-        <div className="grid grid-cols-2 gap-6 md:gap-8">
+        <div className="grid grid-cols-2 gap-6 md:gap-8 mt-6">
           {slideOptions.slice(0, 4).map((option) => (
             <Button
               key={option.id}
               onClick={() => handleOptionClick(option.id)}
-              className="bg-blue-300 hover:bg-blue-400 text-black font-semibold rounded-lg px-6 py-10 flex flex-col items-start space-y-1 shadow-md"
+              className="bg-gradient-to-br from-[#1F2185]/80 to-[#6392DC]/80 hover:from-[#1F2185]/100 hover:to-[#6392DC]/100 text-black font-semibold rounded-lg pl-6 py-10 flex space-y-1 shadow-md cursor-pointer"
             >
               <span className="flex items-center justify-center w-12 h-12 bg-[#6392DC] text-white rounded-full mb-2 shrink-0">
               <Image src={option.icon} alt={`${option.title} icon`} width={30} height={30}></Image>
               </span>
-              <span className="text-lg">{option.title}</span>
-              <span className="text-sm text-[#1e1e1e]">{option.subtitle}</span>
+
+              <div className="flex flex-col justify-center items-start ">
+                <span className="text-2xl">{option.title}</span>
+                <span className="text-base text-[#1e1e1e] font-light">{option.subtitle}</span>
+              </div>
             </Button>
           ))}
         </div>
@@ -41,13 +44,16 @@ export default function AddSlide() {
           <Button 
             key={slideOptions[4].id}
             onClick={() => handleOptionClick(slideOptions[4].id)}
-            className="bg-blue-300 hover:bg-blue-400 text-black font-semibold rounded-lg px-6 py-10 flex flex-col items-start space-y-1 shadow-md"
+            className="bg-gradient-to-br from-[#1F2185]/80 to-[#6392DC]/80 hover:from-[#1F2185]/100 hover:to-[#6392DC]/100 text-black font-semibold rounded-lg px-12 py-10 flex space-y-1 shadow-md cursor-pointer"
           >
             <span className="flex items-center justify-center w-12 h-12 bg-[#6392DC] text-white rounded-full mb-2 shrink-0">
               <Image src={slideOptions[4].icon} alt={`${slideOptions[4].title} icon`} width={30} height={30}></Image>
             </span>
-            <span className="text-lg">{slideOptions[4].title}</span>
-            <span className="text-sm text-[#1e1e1e]">{slideOptions[4].subtitle}</span>
+            <div className="flex flex-col justify-center items-start ">
+              <span className="text-2xl">{slideOptions[4].title}</span>
+              <span className="text-base text-[#1e1e1e] font-light">{slideOptions[4].subtitle}</span>
+
+            </div>
           </Button>
         </div>
       </div>
